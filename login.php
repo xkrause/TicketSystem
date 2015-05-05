9a1df402f0ce6c10fee4a3641754eeb81d59ee5c
ergocraigk <ergocraigk@gmail.com>
2015-05-05 12:37:30 -0700

    quick fix from last upload landaing

--- login.php
+++ login.php
@@ -43,7 +43,7 @@
             if ($count == 1 && $al == "1") {
                 $_SESSION['identity'] = $sqlid;
                 $_SESSION['accessLevel'] = $al;
-                header("Location: techLandaing.php"); 
+                header("Location: techLanding.php"); 
             }
             //if admin go to admin page
             elseif($count == 1 && $al == "2"){
