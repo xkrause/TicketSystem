39e72199c116ad1ced75b69509be76e889de32d0
ergocraigk <ergocraigk@gmail.com>
2015-06-04 11:24:48 -0700

    re uploading viewchanges after adding close ticket email

--- view.php
+++ view.php
@@ -24,7 +24,7 @@
     $id=$_GET['ticketid'];
     
     //sql statment to access the information about the ticket
-    $sql = "SELECT `Tickets`.ticketid, `Tickets`.firstname, `Tickets`.lastname, `Tickets`.urgency, `Tickets`.description, `Tickets`.email, `Tickets`.domain, `Tickets`.`date submitted`, `notes`.note
+    $sql = "SELECT `Tickets`.ticketid, `Tickets`.firstname, `Tickets`.lastname, `Tickets`.urgency, `Tickets`.description, `Tickets`.email, `Tickets`.domain, `Tickets`.`date submitted`, `notes`.note, `Tickets`.pcid, `Tickets`.stateid, `Tickets`.catagories, `Tickets`.assignedtech
             FROM `craigk_ticket` . `Tickets`
             LEFT JOIN `craigk_ticket` . `notes`
             ON `Tickets`.ticketid = `notes`.`ticketid`
@@ -44,35 +44,86 @@
         $dom=$row['domain'];
         $date=$row['date submitted'];
         $notes=$row['note'];
+        $pcid=$row['pcid'];
+        $stid=$row['stateid'];
+        $catagory=$row['catagories'];
+        $technician=$row['assignedtech'];
     }
     
+    //query to get a list of the possible technicians in the system
+    $fixer = "SELECT username FROM `login`";
+    
+    //save the results of the above query
+    $techs = $dbh->query($fixer);
+    
     //if there is a post variable notes....
-    if (!empty($_POST['notes'])){
+    if (!empty($_POST['notes']) or !empty($_POST['pcid']) or !empty($_POST['stid'])){
         
         //gets the server time and formats it for display
         $date = new DateTime();
         $dateFormatted=$date->format('Y-m-d H:i:s');
         
+        //save state id and pc id into variabls to be antered into query later
+
+        
+        //get the current user ID into the page from login to insert into ticket information
         $identity=$_SESSION['identity'];
         foreach( $identity as $value ) {
            $identity=$value;
         }
         
         //save the new posted note to be used in insert statment for the database
-        $scrib=$identity . "--------" . $dateFormatted . "<br>" . $_POST['notes'];
+        $scrib=$identity . "--------" . $dateFormatted;
+        if(!empty($_POST['stid'])){
+            $stid=$_POST['stid'];
+        }
+        if(!empty($_POST['pcid'])){
+            $pcid=$_POST['pcid'];
+        }
+        
+        $scrib=$scrib . "    " . "PCID: " . $pcid . "    " . "StateID: " . $stid . "<br>" . $_POST['notes'];
+        
+        //save posted values for assigned technician and catagory
+        if(!empty($_POST['catagory'])){
+            $catagory=$_POST['catagory'];
+        }
+        if(!empty($_POST['technician'])){
+            $technician=$_POST['technician'];
+        }
         
         //sql statment to insert the above saved notes into the database
         $sql="INSERT INTO `craigk_ticket`.`notes` (note,ticketid)
               VALUES (:scrib,:tid)";
         
+        //sql statment to insert stateid &|| pcid
+        $sql2="UPDATE `craigk_ticket`.`Tickets`
+               SET pcid=:pcid,stateid=:stid
+               WHERE ticketid=$id";
+               
+        //sql statement to update technician and catagory into Tickets table
+        $sql3="UPDATE `craigk_ticket`.`Tickets`
+               SET catagories=:catagory,assignedtech=:technician
+               WHERE ticketid=$id";
+        
         //prepare the PDO for security purposes
         $statement = $dbh->prepare($sql);
+        $statement2 = $dbh->prepare($sql2);
+        $statement3 = $dbh->prepare($sql3);
         
         //attach note to the PDO's and perform the query to insert into the database
         $statement->bindParam(':scrib', $scrib, PDO::PARAM_STR);
         $statement->bindParam(':tid', $tid, PDO::PARAM_STR);
+        $statement2->bindParam(':pcid', $pcid, PDO::PARAM_STR);
+        $statement2->bindParam(':stid', $stid, PDO::PARAM_STR);
+        $statement3->bindParam(':technician', $technician, PDO::PARAM_STR);
+        $statement3->bindParam(':catagory', $catagory, PDO::PARAM_STR);
+        
+        //perform the query
         $statement->execute();
+        $statement2->execute();
+        $statement3->execute();
     }
+    
     //sql query to access the notes of the selected ticket
     $sqlnotes="SELECT `notes`.note FROM `craigk_ticket` . `notes` WHERE ticketid=$id";
     
@@ -111,7 +162,7 @@
     <div class="jumbotron">
         <?php
         echo "<div class='panel panel-default'>";
-        echo "<div class='panel-heading'><h4>Ticket ID:$tid   Urgency:$urg</h4></div>";
+        echo "<div class='panel-heading'><h4>Ticket ID:$tid   Urgency:$urg</h4>\nThis is a $catagory problem assigned to $technician.</div>";
         echo "<div class='panel-body'>$dom $fname $lname: $email</div>";
         echo "<div class='panel-footer'>Description: $des</div></div>";
       
@@ -127,14 +178,36 @@
         ?>
     
            <form action='view.php?ticketid=<?php echo $id; ?>' method='post'>
+                <div class='col-xs-6'>PCID:<input type='text' class="form-control" name='pcid'></div>
+                <div class='col-xs-6'>StateID:<input type='text' class="form-control" name='stid'></div><br>
+                <div class='col-xs-6'>Assign a Technician:
+                    <select name="technician" class="form-control">
+                            <option selected="selected" disabled="disabled" value="">- Assign Technician - </option>
+                            <?php 
+                            foreach($techs as $row){
+                                echo "<option value='$row[username]'>$row[username]</option>";
+                            }
+                            ?>
+                    </select>
+                </div>
+                <div class='col-xs-6'>
+                    Assign category:
+                    <select name='catagory' class='form-control'>
+                      <option value="network">network</option>
+                      <option value="software">software</option>
+                      <option value="hardware">hardware</option>
+                    </select>
+                </div>
+            <br><br>
                 <h4>Notes</h4>
                 <textarea name="notes" rows="8" cols="50" id = "notes" maxlength = "1000" require placeholder="1000 character limit." class="form-control"></textarea>
                 <div id="textarea_feedback"></div>
                 <br>
                 <input name="submit" type="submit" value="Submit" class="btn btn-default" style='float: right;'>
+            </div>
            </form>
            <br>
-           <a href='closeTicket.php?ticketid=<?php echo $id; ?>'><button class="btn btn-default" onclick="closeConfirm()">Close Ticket</button></a>
+           <a href='closeTicket.php?ticketid=<?php echo $id; ?>'><button class="btn btn-default">Close Ticket</button></a>
            <a href='<?php if($_SESSION['accessLevel'] == '1'){
                               echo "techLanding.php";
                           }
@@ -142,9 +215,8 @@
                               echo "admin.php";
                           }
                     ?>'><button class="btn btn-default" style='float: left;'>Return</button></a>
-           <br> <br>
-           
-           <script>
+           <br><br>
+	           <script>
 				//The confirmation box
 				function closeConfirm() {
                                     <?php
@@ -165,20 +237,6 @@
 						}				 
 					}
 				}
-			</script>  
-    <!--Lets leave this in place for later if we get to it. Right now this is another
-         sql statment and not as critical
-        <form>
-            Please choose a category:
-            <select>
-              <option value="option1">Problem 1</option>
-              <option value="option2">Problem 2</option>
-              <option value="option3">Problem 3</option>
-              <option value="option4">Problem 4</option>
-              <option value="option5">Problem 5</option>
-            </select>
-        </form>
-            <!--This is a problem i think because we already have a form on the page this isn't critical so we should hold it off for later-->
-            <!--<form> Assigned Technician: <input type="text" name="TechName"></form>-->
+			</script>
     </div>
 </body>
